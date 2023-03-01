0b8462e590aedc14e1b30a253819b242a22824b8
Nikolay Michel <61086458+NikolayTheSTAR@users.noreply.github.com>
2023-03-01 17:38:03 +0500

    Update drop fly

--- Assets/Scripts/World/DropItemsContainer.cs
+++ Assets/Scripts/World/DropItemsContainer.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace World
@@ -14,21 +15,30 @@ namespace World
             _playerDropReceiver = playerDropReceiver;
         }
         
-        public void DropItemToPlayer(ResourceItem itemPrefab, Vector3 pos)
+        public void DropItemToPlayer(ResourceItem itemPrefab, Vector3 startPos)
         {
-            var item = Instantiate(itemPrefab, pos, Quaternion.identity, transform);
+            var item = Instantiate(itemPrefab, startPos, Quaternion.identity, transform);
             item.transform.localScale = Vector3.zero;
             
-            LeanTween.scale(item.gameObject, Vector3.one, 0.2f).setOnComplete(() =>
+            LeanTween.scale(item.gameObject, Vector3.one, 0.2f).setOnComplete(() => { LeanTween.value(0, 1, DropWaitTime).setOnComplete(FlyToPlayer);});
+
+            void FlyToPlayer()
             {
-                LeanTween.value(0, 1, DropWaitTime).setOnComplete(() =>
+                //LeanTween.move(item.gameObject, _playerDropReceiver.transform.position, FlyToReceiverTime)
+                LeanTween.value(0, 1, FlyToReceiverTime).setOnUpdate((value) =>
                 {
-                    LeanTween.move(item.gameObject, _playerDropReceiver.transform.position, FlyToReceiverTime).setOnComplete(() =>
-                    {
-                        Destroy(item.gameObject);
-                    });
+                    var difference = _playerDropReceiver.transform.position - startPos;
+                    item.transform.position = startPos + value * (difference);
+
+                    var dopValueY_Use = (value * value - value)*2;
+                    var dopValueY = Math.Abs(dopValueY_Use * difference.x);
+                    item.transform.position += new Vector3(0, dopValueY, 0);
+
+                }) .setOnComplete(() =>
+                {
+                    Destroy(item.gameObject);
                 });
-            });
+            }
         }
     }
 
