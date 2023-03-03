94c90192b7661c09a4dce37d8838da911e09a27d
Nikolay Michel <61086458+NikolayTheSTAR@users.noreply.github.com>
2023-03-03 16:55:05 +0500

    animate counters

--- Assets/Scripts/TheSTAR/GUI/ItemCounter.cs
+++ Assets/Scripts/TheSTAR/GUI/ItemCounter.cs
@@ -11,13 +11,33 @@ namespace TheSTAR.GUI
         [SerializeField] private Image iconImage;
         [SerializeField] private TextMeshProUGUI counterText;
 
+        private int _animLTID = 0;
+        private int _currentValue = 0;
+
         public void SetValue(int value)
         {
-            counterText.text = value.ToString();
-        
-            // todo: animate
-        
             gameObject.SetActive(value > 0);
+            
+            counterText.text = value.ToString();
+
+            bool needAnimate = value > _currentValue;
+            _currentValue = value;
+
+            if (needAnimate)
+            {
+                if (_animLTID != -1)
+                {
+                    LeanTween.cancel(_animLTID);
+                    _animLTID = -1;
+                }
+                
+                _animLTID =
+                LeanTween.scale(counterText.gameObject, new Vector3(1.2f, 1.2f, 1), 0.1f).setOnComplete(() =>
+                {
+                    _animLTID =
+                    LeanTween.scale(counterText.gameObject, Vector3.one, 0.2f).id;
+                }).id;
+            }
         }
     }
 }
